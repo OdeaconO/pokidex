23beede656354ac35dffc7d27e50eef95fc8137a
OdeaconO <dhruvrai2903@gmail.com>
2025-09-18 02:19:57 +0530

    removed vite pwa and caching

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["vite.svg"], // you can add other assets too
-      manifest: {
-        name: "Pokiidex",
-        short_name: "Pokiidex",
-        description: "A Pokémon app built with Vite + React",
-        theme_color: "#d10000ff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: ".",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
